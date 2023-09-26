f2b2f48796649430a87e5383d6dd2a0e135d2cb1
Dev Chauhan <devchauhan62003@gmail.com>
2023-09-27 00:56:20 +0530

    Added GET Route for products & used useState and useEffect

--- React/my-project/src/pages/AddProduct.js
+++ React/my-project/src/pages/AddProduct.js
@@ -1,16 +1,18 @@
 import AddProductForm from "../components/product/AddProductForm";
+import { useNavigate } from "react-router-dom";
 
 function AddProduct() {
-  function addProductHandler(newProduct) {
 
+  const navigate = useNavigate();
+  
+  function addProductHandler(newProduct) {
     fetch("http://localhost:3004/product", {
       method: "POST",
       body: JSON.stringify(newProduct),
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(response => response.json())
-      .then(data => console.log(data))
+    }).then(response => navigate("/",{replace:true}))
       .catch(err => console.log(err));
   }
   return <AddProductForm addProductHandler={addProductHandler} />;
--- React/my-project/src/pages/Product.js
+++ React/my-project/src/pages/Product.js
@@ -1,29 +1,39 @@
 import React from 'react'
-import ProductList from '../components/product/ProductList';
+import ProductList from '../components/product/ProductList'
 
-function Product()
-{
-    let products = [
-        {
-            id:"p1",
-            product_name:"City Tour",
-            image:"https://globaltravelexchange.com/public/upload/city/20822/sightseeing/4539_1591615717.jpg",
-            description:"It is a good tour",
-            price:"20000"
-        },
-        {
-            id:"p2",
-            product_name:"Forest Tour",
-            image:"https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0e/58/39/85.jpg",
-            description:"It is a refreshing tour",
-            price:"50000"
-        }
-    ];
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+function Product() {
+
+    let [products, setProducts] = useState([]);
+    let [isDataFetching, setisDataFetching] = useState(true);
+
+    useEffect(() => {
+        setisDataFetching(true)
+        fetch("http://localhost:3004/product")    //GET 
+            .then(response => response.json())
+            .then(responseData => {
+                setisDataFetching(false);
+                setProducts(responseData.data)
+            })
+            .catch(err => console.log(err));
+
+    }, [])
+
+
+
+    if (isDataFetching) {
+        return <div>
+            Data is Loading
+        </div>
+    }
     return (
         <div>
-            <ProductList products={products}/> 
+            <ProductList products={products} />
         </div>
-      )
+    )
 }
 
 export default Product
