65276adb17922c271f21cc7e8ca59d202dc0a92b
Dev Chauhan <devchauhan62003@gmail.com>
2023-09-28 19:37:04 +0530

    Achieved Shared state by 'Lifting state up' technique

--- Projects/tic-tac-toe/src/App.js
+++ Projects/tic-tac-toe/src/App.js
@@ -1,25 +1,29 @@
 import Square from './components/Square'
 import styles from './App.css'
+import { useState } from 'react'
 
 export default function Board() {
+
+  const [squares,setSquare] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
 
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[3]}/>
+      <Square value={squares[4]}/>
+      <Square value={squares[5]}/>
       </div>
 
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squares[6]}/>
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>
       </div>
     </>
   )
--- Projects/tic-tac-toe/src/components/Square.js
+++ Projects/tic-tac-toe/src/components/Square.js
@@ -1,14 +1,9 @@
 import styles from '../App.css'
 import {useState} from 'react'
 
-function Square() {
-    
-    const [value , setValue] = useState(null)
+function Square({value}) {
 
-    function handleClick() {
-     setValue('X');
-    }
-    return <button className="square" onClick={handleClick}>{value}</button>
+    return <button className="square" >{value}</button>
 }
 
 export default Square;
