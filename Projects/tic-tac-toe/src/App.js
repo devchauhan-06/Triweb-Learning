501e26437d522e3cac9279dc548b8cbb1ec839b3
Dev Chauhan <devchauhan62003@gmail.com>
2023-09-29 19:29:18 +0530

    Added Time Travel

--- Projects/tic-tac-toe/src/App.js
+++ Projects/tic-tac-toe/src/App.js
@@ -2,10 +2,7 @@ import Square from './components/Square'
 import styles from './App.css'
 import { useState } from 'react'
 
-export default function Board() {
-
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquare] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
 
@@ -21,8 +18,7 @@ export default function Board() {
       nextSquares[i] = 'O';
     }
 
-    setSquare(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -58,6 +54,52 @@ export default function Board() {
   )
 }
 
+export default function Game() {
+
+  const xIsNext = currentMove % 2 === 0;
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const currentSquares = history[currentMove];
+
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    }
+    else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
