f14ad6764d91cadf0dbced729b797e245cd7967b
Dev Chauhan <devchauhan62003@gmail.com>
2023-10-12 15:08:46 +0530

    Changed README.md of HTML

--- Projects/portfolio/src/App.js
+++ Projects/portfolio/src/App.js
@@ -1,4 +1,4 @@
-import { Routes , Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -8,9 +8,9 @@ function App() {
   return (
     <Routes>
 
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/contact" element={<Contact/>}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
 
     </Routes>
   );
