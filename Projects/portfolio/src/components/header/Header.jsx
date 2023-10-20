49e7e1ed2a0091ce308ac412a00bf5b3a5aa494d
Dev Chauhan <devchauhan62003@gmail.com>
2023-10-21 00:53:17 +0530

    Created a responsive Home Component

--- Projects/portfolio/src/components/header/Header.jsx
+++ Projects/portfolio/src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className='nav container'>
-                <a href="index.html" className='nav__logo'>Dev</a>
+                <a href="index.html" className='nav__logo'>PORTFOLIO</a>
 
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
---
+++ Projects/portfolio/src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/devchauhan_06/" className="home__social-icon" target='_blank'>
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/dev-chauhan-437900257/" className="home__social-icon" target='_blank'>
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/devchauhan-06" className="home__social-icon" target='_blank'>
+                <i className="uil uil-github-alt"></i>
+            </a>
+
+
+        </div>
+    )
+}
+
+export default Social
