749866fa91c8a7006e85bb687bb33f5dd1999274
Dev Chauhan <devchauhan62003@gmail.com>
2023-10-20 15:10:38 +0530

    Added general CSS and created a responsive collapsable navbar

--- Projects/portfolio/src/App.js
+++ Projects/portfolio/src/App.js
@@ -1,22 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import styles from './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navigation from "./components/Navigation";
+import React from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+
 
 function App() {
   return (
-    // <Routes>
-
-    //   <Route path="/" element={<Home />}></Route>
-    //   <Route path="/about" element={<About />}></Route>
-    //   <Route path="/contact" element={<Contact />}></Route>
-
-    // </Routes>
     <>
-      <Navigation />
+  <Header/>
     </>
   );
 }
