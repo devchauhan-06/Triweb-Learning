faeafc5f101791ae1d9fa0add07478056005455e
Dev Chauhan <devchauhan62003@gmail.com>
2023-10-21 18:42:28 +0530

    Created a Services Component with custom collapsable modal

---
+++ Projects/portfolio/src/components/services/Services.jsx
@@ -0,0 +1,146 @@
+import React, { useState } from 'react'
+
+import './services.css'
+
+const Services = () => {
+
+    const [toggleState, setToggleState] = useState(0);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+    return (
+        <section className="services section" id="services">
+            <h2 className="section__title">Services</h2>
+            <span className="section__subtitle">What I Offer</span>
+
+            <div className="services__container container grid">
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-web-grid services__icon"></i>
+                        <h3 className="services__title">Frontend <br /> Designer</h3>
+                    </div>
+
+                    <span className="services__button" onClick={() => toggleTab(1)} >View More  <i className="uil uil-arrow-right services__button-icon"></i> </span>
+
+                    <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">Frontend Designer</h3>
+                            <p className="services__modal-description">I can create modern Frontend</p>
+
+                            <ul className="services__modal-services grid">
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">I develop User Interface</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">Web page development</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">I develop create UX element interaction</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">I can position your company brand</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">I can Design mockups and wireframes for companies</p>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-arrow services__icon"></i>
+                        <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(2)} className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i> </span>
+
+                    <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">UI/UX Designer</h3>
+                            <p className="services__modal-description">I can create attractive & modern UI/UX</p>
+
+                            <ul className="services__modal-services grid">
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">My designs are aimed at reducing cognitive load and ensuring users can easily navigate your platform</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">I specialize in crafting interfaces that are simple, intuitive, and visually appealing. </p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> My UI/UX service adheres to accessibility guidelines and standards, ensuring that your interface is usable by all, including individuals with disabilities.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services__content">
+                    <div>
+                        <i className="uil uil-edit services__icon"></i>
+                        <h3 className="services__title">Backend <br /> Designer</h3>
+                    </div>
+
+                    <span onClick={() => toggleTab(3)} className="services__button">View More <i className="uil uil-arrow-right services__button-icon"></i> </span>
+
+                    <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+                        <div className="services__modal-content">
+                            <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                            <h3 className="services__modal-title">Backend Designer</h3>
+                            <p className="services__modal-description"> I specialize in crafting backend solutions that are simple, intuitive, and visually appealing.</p>
+
+                            <ul className="services__modal-services grid">
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> I prioritize understanding their needs and preferences to create an experience that resonates with them.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">My designs are aimed at reducing cognitive load and ensuring users can easily interact with your platform.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info">My backend service guarantees that your digital presence is responsive across all devices.</p>
+                                </li>
+
+                                <li className="services__modal-service">
+                                    <i className="uil uil-check-circle services__modal-icon"></i>
+                                    <p className="services__modal-info"> I'll help you gather user feedback and continually refine your backend to enhance usability, rectify pain points, and keep your systems in sync with evolving user expectations.</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Services
